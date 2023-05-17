9cf298ee8aedf9c30ea0b206e6054a54f357fd26
hsin-rong <85164994+hsin-rong@users.noreply.github.com>
2023-05-17 20:28:25 +0800

    logo size change

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -29,47 +29,43 @@ export default function Header({ title, slogan }) {
         <div className={styles.container}>
             <div className={styles.header}>
                 <div className={styles.headerWwrap}>
-
-      <div className={styles.Drawer}>
-
-      {/* <Button type="Text" onClick={showDrawer} icon={<RxHamburgerMenu/>}></Button> */}
-      <RxHamburgerMenu className={styles.Drawericon} onClick={showDrawer}/>
-
-      <Drawer
-        className={styles.Drawer}
-        width={317}
-        onClose={onClose}
-        placement={"left"}
-        open={open}
-        bodyStyle={{
-          paddingBottom: 80,
-          backgroundColor:"#FCF3F6",
-        }}
-        extra={
-            <Space>
-              <Button type="Text" onClick={onClose}></Button>
-              <img src="/images/logo3.png" className={styles.dlogo} ></img>
-            </Space>
-        }
+                    <div className={styles.Drawer}>
+                        {/* <Button type="Text" onClick={showDrawer} icon={<RxHamburgerMenu/>}></Button> */}
+                        <RxHamburgerMenu className={styles.Drawericon} onClick={showDrawer}/>
+                        <Drawer
+                            className={styles.Drawer}
+                            width={317}
+                            onClose={onClose}
+                            placement={"left"}
+                            open={open}
+                            bodyStyle={{
+                                paddingBottom: 80,
+                                backgroundColor:"#FCF3F6",
+                            }}
+                            extra={
+                                <Space>
+                                    <Button type="Text" onClick={onClose}></Button>
+                                    <img src="/images/logo3.png" className={styles.dlogo} ></img>
+                                </Space>
+                            }
       >
-            <Link to="/">
+                            <Link to="/">
                <div className={styles.nav}>首頁</div>
-            </Link>
-            <Link to="/">
+                            </Link>
+                            <Link to="/">
                 <div className={styles.nav}>最新消息</div>
-            </Link>
-            <Link to="/albums">
+                            </Link>
+                            <Link to="/albums">
                  <div className={styles.nav}>商品一覽</div>
-            </Link>
-            <Link to="/">
+                            </Link>
+                            <Link to="/">
                 <div className={styles.nav}>預購專區</div>
-            </Link> 
-            <Link to="/">
+                            </Link> 
+                            <Link to="/">
                 <div className={styles.nav}>單曲排行</div>
-            </Link>
-      </Drawer>
-    </div>
-
+                            </Link>
+                        </Drawer>
+                    </div>
                     {/* <HamburgerMenu
                         onClick={() => setIsOnTouch(!isOnTouch)}
                         isOnTouch={isOnTouch}
@@ -78,30 +74,26 @@ export default function Header({ title, slogan }) {
                         <img src="/images/logo3.png" className={styles.logo}></img>
                     </Link>
                 </div>
-
                 <div className={styles.navContentLeft}>
-                        <Link to="/">
+                    <Link to="/">
                             <div className={styles.navItems}>首頁</div>
-                        </Link>
-                        <Link to="/">
+                    </Link>
+                    <Link to="/">
                             <div className={styles.navItems}>最新消息</div>
-                        </Link>
-                        <Link to="/albums">
+                    </Link>
+                    <Link to="/albums">
                             <div className={styles.navItems}>商品一覽</div>
-                        </Link>
-                        <Link to="/">
+                    </Link>
+                    <Link to="/">
                             <div className={styles.navItems}>預購專區</div>
-                        </Link> 
-                        <Link to="/">
-                           <div className={styles.navItems}>單曲排行</div>
-                        </Link>
-                 </div> 
-              
+                    </Link> 
+                    <Link to="/">
+                        <div className={styles.navItems}>單曲排行</div>
+                    </Link>
+                </div> 
                 {/* <NavBar open={isOnTouch} onClose={() => setIsOnTouch(false)} /> */}
             </div>
-
             <img src="/images/separtionBar.png" className={styles.line}></img>
-           
             <div className={styles.iconWrap}>
                 {/* <SetColorMode/> */}
                 <div className={styles.lefttop}>
