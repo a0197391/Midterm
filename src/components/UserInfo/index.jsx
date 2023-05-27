bed06cc29debd175324abb12de82bd82f6267ef8
VickyChang <a0197391@gmail.com>
2023-05-28 00:54:20 +0800

    try login

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -1,23 +1,55 @@
-import { UserSwitchOutlined } from '@ant-design/icons';
-import {BsPersonCircle } from "react-icons/bs";
+// import { UserSwitchOutlined } from '@ant-design/icons';
+// import {BsPersonCircle } from "react-icons/bs";
+// import { useNavigate } from 'react-router-dom';
+// import styles from "./userinfo.module.css";
+
+// export default function UserInfo(props) {
+//    const navigate = useNavigate();
+  
+//    const goToProfile = () => {
+//       navigate("/auth/login?redirect=/auth/profile");
+//    };
+
+
+//    return (
+//       <nav onClick={goToProfile} style={{ ...props.style }} className={styles.userInfo} >
+//          <BsPersonCircle className={styles.userInfoOutlined} />
+//          <p className={styles.userInfoText}>
+//             請登入
+//          </p>
+//       </nav>
+
+//    );
+// }
+
+import { UserOutlined, UserSwitchOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
+import { useUserInfo } from "../../react-query";
 import styles from "./userinfo.module.css";
 
 export default function UserInfo(props) {
+   const { data: userInfo} = useUserInfo();
    const navigate = useNavigate();
-  
+
    const goToProfile = () => {
-      navigate("/auth/login?redirect=/auth/profile");
+      if(userInfo?.name)
+         navigate("/auth/profile")
+      else
+         navigate("/auth/login?redirect=/auth/profile");
    };
 
-
    return (
-      <nav onClick={goToProfile} style={{ ...props.style }} className={styles.userInfo} >
-         <BsPersonCircle className={styles.userInfoOutlined} />
+      <div onClick={goToProfile} style={{ ...props.style }} className={styles.userInfo} >
+         {userInfo
+            ? <UserOutlined className={styles.userInfoOutlined} />
+            : <UserSwitchOutlined className={styles.userInfoOutlined} />
+         }
          <p className={styles.userInfoText}>
-            請登入
+            {!!userInfo?.name
+               ? `${userInfo.name}'s`
+               : `請登入`
+            }
          </p>
-      </nav>
-
+      </div>
    );
 }
