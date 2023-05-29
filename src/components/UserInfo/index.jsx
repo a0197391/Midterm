d974c8945dc659f76f2cecf1cafa8e9ba0a55065
VickyChang <a0197391@gmail.com>
2023-05-29 14:56:14 +0800

    fix firebase

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -22,6 +22,40 @@
 //    );
 // }
 
+// import { UserOutlined, UserSwitchOutlined } from '@ant-design/icons';
+// import { useNavigate } from 'react-router-dom';
+// import { useUserInfo } from "../../react-query";
+// import styles from "./userinfo.module.css";
+// import {BsPersonCircle } from "react-icons/bs";
+
+// export default function UserInfo(props) {
+//    const { data: userInfo} = useUserInfo();
+//    const navigate = useNavigate();
+
+//    const goToProfile = () => {
+//       if(userInfo?.name)
+//          navigate("/auth/profile")
+//       else
+//          navigate("/auth/login?redirect=/auth/profile");
+//    };
+
+//    return (
+//       <div onClick={goToProfile} style={{ ...props.style }} className={styles.userInfo} >
+//          {userInfo
+//             ? <BsPersonCircle className={styles.userInfoOutlined} />
+//             : <BsPersonCircle className={styles.userInfoOutlined} />
+//          }
+//          <p className={styles.userInfoText}>
+//             {!!userInfo?.name
+//                ? `${userInfo.name}'s`
+//                : `請登入`
+//             }
+//          </p>
+//       </div>
+//    );
+// }
+
+
 import { UserOutlined, UserSwitchOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 import { useUserInfo } from "../../react-query";
@@ -40,17 +74,19 @@ export default function UserInfo(props) {
    };
 
    return (
+
       <div onClick={goToProfile} style={{ ...props.style }} className={styles.userInfo} >
          {userInfo
-            ? <BsPersonCircle className={styles.userInfoOutlined} />
+            ? <BsPersonCircle className={styles.userInfoOutlined}/>
             : <BsPersonCircle className={styles.userInfoOutlined} />
          }
          <p className={styles.userInfoText}>
             {!!userInfo?.name
-               ? `${userInfo.name}'s`
+               ? `${userInfo.name}`
                : `請登入`
             }
          </p>
       </div>
+
    );
 }
